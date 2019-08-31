0108e80309bd5b6f62183130ace05514395231ea
lee95292 <lee95292@naver.com>
2019-08-31 16:16:12 +0900

    testcase modified

--- src/test/java/kr/ac/jbnu/jclip/service/JBNUClipServiceTest.java
+++ src/test/java/kr/ac/jbnu/jclip/service/JBNUClipServiceTest.java
@@ -6,10 +6,10 @@ import org.jsoup.nodes.Element;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith( SpringJUnit4ClassRunner.class )
-@SpringBootTest
+@DataJpaTest
 public class JBNUClipServiceTest {
 	private JBNUClipService clipService;
 	
