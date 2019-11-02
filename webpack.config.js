2b11dbb8fcf7c6dc19580c4ea882262cdd3666b0
lee95292 <lee95292@naver.com>
2019-11-03 00:59:53 +0900

    [Setting]front설정 세팅 완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
 //https://start.goodtime.co.kr/2018/09/%ec%8a%a4%ed%94%84%eb%a7%81-%eb%b6%80%ed%8a%b8-%eb%a6%ac%ec%95%a1%ed%8a%b8-%ea%b0%9c%eb%b0%9c-%ec%85%8b%ec%97%85-2018/ 
 //webpack config 셋업 참고링크
 var path =require('path');
+const getLogger=require('webpack-log');
+const log=getLogger({name:'webpack-batman'});
 
 module.exports={
-    context:path.resolve(__dirname,'src/main/webapp/feapps'),
-
-    entry:'./src/index.js',
+    // context:path.resolve(__dirname,'src/main/'),
+    
+    entry:path.resolve(__dirname,'src/main/webapp/feapps/src/index.js'),
     devtool:'sourcemaps',
     cache:true,
     output:{
-        filename:'./src/main/resources/static/js/react/[name].bundle.js',
-        path:path.resolve(__dirname,'')
+        // path:path.resolve(__dirname,'src/main/resources/static/js/'),
+        // filename:'[name].bundle.js',
+        path:__dirname,
+        filename:'./src/main/resources/static/js/[name].bundle.js'
     },
     mode:'none',
+    devServer:{
+        index:path.resolve(__dirname,'src/main/webapp/WEB-INF/jsp/index.jsp'),
+        host:'localhost',
+        contentBase:path.resolve(__dirname,'src/main/webapp/WEB-INF/'),
+        port:9000
+    },
     module:{
         rules:[
             {
@@ -26,7 +36,7 @@ module.exports={
                 }
             },
             {
-                test:/\.js?$/,
+                test:/\.jsx?$/,
                 exclude:/(node_modules)/,
                 use:{
                     loader:'babel-loader'
@@ -39,3 +49,12 @@ module.exports={
         ]
     }
 }
+
+log.info('outputPath:'+module.exports.output.path);
+log.info('devserver contentbase:'+module.exports.devServer.contentBase);
+log.info(module.exports.entry.path);
+
+// log.info('Jingle Bells, Batman Smells');
+// log.warn('Robin laid an egg');
+// log.error('The Batmobile lost a wheel');
+// log.debug('And the Joker got away');
