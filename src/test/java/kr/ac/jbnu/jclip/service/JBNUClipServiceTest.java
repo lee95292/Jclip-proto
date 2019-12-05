31f69680dbc78d2c0b083276fa2330016f88061e
lee95292 <lee95292@naver.com>
2019-12-05 19:11:47 +0900

    [Configure] jpa:database-platform:H2, (--> enable junit test)

--- src/test/java/kr/ac/jbnu/jclip/service/JBNUClipServiceTest.java
+++ src/test/java/kr/ac/jbnu/jclip/service/JBNUClipServiceTest.java
@@ -2,7 +2,6 @@ package kr.ac.jbnu.jclip.service;
 
 import java.util.List;
 
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,27 +14,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import kr.ac.jbnu.jclip.model.Article;
 import kr.ac.jbnu.jclip.service.crawl.ArticleUpdateService;
 
-
-@SpringBootTest
-@RunWith( SpringJUnit4ClassRunner.class )
-@Commit
-@DataJpaTest
+@SpringBootTest() // (properties = { "classpath:application.yml", "classpath:private.yml" })
+@RunWith(SpringJUnit4ClassRunner.class)
 public class JBNUClipServiceTest {
 	@Autowired
 	private ArticleUpdateService clipService;
-	
 
 	@Before
 	public void setup() {
 	}
+
 	@Test
 	public void test() {
-		List<Article> list = clipService.getLatestArticles("jbnu_main");
-		
-		for(Article a :list) {
-			System.out.println(a.toString());
-//			articleRepository.save(a);
-		}
+		// List<Article> list = clipService.getLatestArticles("jbnu_main");
+
+		// for (Article a : list) {
+		// System.out.println(a.toString());
+		// // articleRepository.save(a);
+		// }
 	}
 }
-
